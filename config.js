854873c5d3154547f009d0eb979fe807b4f24705
Chris <oldclock@gmail.com>
2025-05-28 23:48:43 +0800

    update channels

--- config.js
+++ config.js
@@ -59,10 +59,53 @@ const YOUTUBE_CONFIG = {
     "UCWQGiZK_rAMpnl4awiUrqqg", // Ratio Yuires【Project:YuiRes】
     "UCX6x7k9z83gw7DQYfLNTOmw", // スイミー / Swimmy
     "UCt30jJgChL8qeT9VPadidSw", // しぐれうい
+    // T2 - EN/ID
+    "UC_sFNM0z0MWm9A6WlKPuMMg", // Hololive # Nerissa Ravencroft Ch. hololive-EN
+    "UCP0BspO_AMEe3aQqqpo89Dg", // Hololive # Moona Hoshinova hololive-ID
+    "UCa2e3c0SDA5aeFTROIPz9eA", // Xiulan Long Ch.
+    "UCgZRsBwbdUzxKeOOmmJIXfg", // The Coffee Elf
+    "UCBZ9K3sr0ViQU3p4bI36cmQ", // Jaliz
+    // T2 - ZH
+    "UCUEXMpxIa7le_CK6yYRAboA", // 子午 # 祈Iruni Channel
+    "UCk-n2qPASA48IPgtYAsLYJg", // 子午 # 玥Itsuki Channel
+    "UC6CpUl2thiDslRorMhd00HA", // 哇啦咚咚 # Luxida ch. 露熙妲
+    "UCkiM6bCVlAGRkRm9R_CKbng", // 哇啦咚咚 # Seri ch.星璃
+    "UCLeyYlqnD5k1fbZIIE4eTsg", // 春魚 # Oumua ch. 埃穆亞
+    "UCFiIsVOC1p_gfTYDYXXfl4g", // 春魚 # Nemesis ch. 涅默
+    "UCwzpXmWAFEVKH3VzwvSlY_w", // 春魚 # Earendel ch. 厄倫蒂兒
+    "UCIf6cffSRZqS7TUXbUAK_hw", // 春魚 # Cygnus ch. 熙歌
+    "UCXyAitTabBsoRclTn_Gq-eA", // 春魚 # xxhacucoxx ch. 白白虹
+    "UChJo7dFfZ6mJXQgpXr3OxGg", // 春魚 # Kumosuki ch. 雲隙光
+    "UCvglsaXuC9oHuDJZaZbs0AQ", // 春魚 # Eisnebel ch. 冰霧
+    "UCfZofhrZ4pxCESrcs5m5NJg", // 春魚 # Olda ch. 歐妲
+    "UCxYkBSXKRgbxVwwqBQ_sy0w", // 雲際線 # 角蓮Caren
+    "UCZHa6yKnBnU34yRyvV3EXSA", // 預見 # 瓦西瓦瓦 Vaswawa
+    "UCuPHlMEd0cR-tvAYPjGWVwQ", // 預見 # 詩雨蔻達 Shiu Coda Ch.
+    "UCChAHq4kdRZ0FJ1Jfjvr9cw", // 預見 # 帕蘿妮 Paroniie Ch.
+    "UCuUdD_1DfNlMNF-o2bxvO7Q", // Remiria Ch. 蕾米利亞
+    "UC3rmrlimhWzTsJ1BxY6-p1w", // 水縹そまる / somaru
+    "UCu06qIis7ypubiMrH-3gRNg", // CY Future # 冬雲凜Samui Channel
+    "UCK-H5yv8OpCce5xmUrbOP8A", // 花遊 # 天櫻咲樂さくら【Stu. Huayu】
+    "UCmKy7mz6tRLv7OFdSqbAkrg", // SLSMusic
+    "UCadrQyULVMygKkKGJkZhUYw", // 杉山参 SugiyamaMairu ch.
+    "UCz74f1swTzZAraJjZNyF-8g", // 諾鴉・奈菲墨【VerseLink】
+    "UCdqjKrxciua397Fo4h6xngA", // 狸咪 Tanuki Miihi Ch
+    "UCOseNFJYQJVDB-9D3egG4ZA", // 白月心宿shuku【KOZMII / 比鄰星域所屬】
+    "UCdesGlD7t5JV-z28p5GqVIw", // 名雪薇薇·ヴィヴィch.
+    "UCqGLWrvVOqtrpxFdoTY5tYg", // 柴崎楓音 / Shibasaki Fuune
+    "UCkcenwQpQwgNkG2sl28Az-A", // Devilith Violustre
+    "UC88ZUTjfOK5RFcw-4ar1VPA", // Mischeif Ch. 蜜絲雪芙
+    "UCuXVZoBEw8v4f4TIBpQRTBQ", // 蘭音Reine
+    "UCyqHGoPxqHzRe9ZUtJux7TQ", // 魔女ルンナ I 魔女Runna 🔮🌕 I JP & HKVtuber
+    "UCkTfvQqiCDniJt2XZFEXAbg", // 心咲 ⁄ ᴋᴏᴇ ∅.･
+    "UCnyLvZQKHI71LchdRAkGa_w", // Riru Ch. 記菓莉蕗
+    "UCsJBSOyM1ZCwnANvwTBWmww", // Coxib 可希
+    "UCDb47NT3QzoCiorDtK9C_qg", // ACGlive # 蕾兒 Rayer
+    "UCs-gepebQQqBMiNksqP9cYQ", // ACGlive # 綾音 Ring
+    "UCCiW5TMQLWkSh1hY8RaF85A", // 毬呢Showey Ch.
+    "UC6uZRWNuCa6XB4oRHApdtZw", // 女巫西亞Cya Channel
     // 'UCe_8qM8Wk4cU5oD3WO07gZg', // Shairu.chしゃいる
-    // 'UCR6qhsLpn62WVxCBK1dkLow', // Enna Alouette【NIJISANJI EN】
     // 'UCngjNluk0InGM3RShD_J_cw', // ネモ・テルミナス Nemo Terminus
-    // 'UC0HX1e5jJnhN5Xn0epV2wzA', // MEDA
     // 'UCIf6cffSRZqS7TUXbUAK_hw', // Cygnus ch. 熙歌
   ],
 
